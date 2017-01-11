18df4a6d6e4cae5323c03c21a080bd35d2335243
Juhezi <juhezix@163.com>
2017-01-11 17:11:45 +0800

    complete the DrawLayout in MainActivity

---
+++ app/src/main/java/com/juhezi/coderslife/SingleFragmentActivity.java
@@ -0,0 +1,82 @@
+package com.juhezi.coderslife;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by qiao1 on 2017/1/11.
+ */
+public abstract class SingleFragmentActivity<T extends ViewDataBinding> extends AppCompatActivity {
+    private static String TAG = "SingleFragmentActivity";
+
+    protected void init(T binding) {
+    }
+
+    protected abstract Fragment getFragment();
+
+    @LayoutRes
+    protected abstract int getActLayoutRes();
+
+    protected abstract int getFragContainerId();
+
+    protected Fragment fragment = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        T binding = DataBindingUtil.setContentView(this, getActLayoutRes());
+        fragment = getSupportFragmentManager().findFragmentById(getFragContainerId());
+        if (fragment == null) {
+            fragment = getFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(getFragContainerId(), fragment)
+                    .commit();
+        }
+        init(binding);
+    }
+
+    /**
+     *               ii.                                         ;9ABH,
+     *              SA391,                                    .r9GG35&G
+     *              &#ii13Gh;                               i3X31i;:,rB1
+     *              iMs,:,i5895,                         .5G91:,:;:s1:8A
+     *               33::::,,;5G5,                     ,58Si,,:::,sHX;iH1
+     *                Sr.,:;rs13BBX35hh11511h5Shhh5S3GAXS:.,,::,,1AG3i,GG
+     *                .G51S511sr;;iiiishS8G89Shsrrsh59S;.,,,,,..5A85Si,h8
+     *               :SB9s:,............................,,,.,,,SASh53h,1G.
+     *            .r18S;..,,,,,,,,,,,,,,,,,,,,,,,,,,,,,....,,.1H315199,rX,
+     *          ;S89s,..,,,,,,,,,,,,,,,,,,,,,,,....,,.......,,,;r1ShS8,;Xi
+     *        i55s:.........,,,,,,,,,,,,,,,,.,,,......,.....,,....r9&5.:X1
+     *       59;.....,.     .,,,,,,,,,,,...        .............,..:1;.:&s
+     *      s8,..;53S5S3s.   .,,,,,,,.,..      i15S5h1:.........,,,..,,:99
+     *      93.:39s:rSGB@A;  ..,,,,.....    .SG3hhh9G&BGi..,,,,,,,,,,,,.,83
+     *      G5.G8  9#@@@@@X. .,,,,,,.....  iA9,.S&B###@@Mr...,,,,,,,,..,.;Xh
+     *      Gs.X8 S@@@@@@@B:..,,,,,,,,,,. rA1 ,A@@@@@@@@@H:........,,,,,,.iX:
+     *     ;9. ,8A#@@@@@@#5,.,,,,,,,,,... 9A. 8@@@@@@@@@@M;    ....,,,,,,,,S8
+     *     X3    iS8XAHH8s.,,,,,,,,,,...,..58hH@@@@@@@@@Hs       ...,,,,,,,:Gs
+     *    r8,        ,,,...,,,,,,,,,,.....  ,h8XABMMHX3r.          .,,,,,,,.rX:
+     *   :9, .    .:,..,:;;;::,.,,,,,..          .,,.               ..,,,,,,.59
+     *  .Si      ,:.i8HBMMMMMB&5,....                    .            .,,,,,.sMr
+     *  SS       :: h@@@@@@@@@@#; .                     ...  .         ..,,,,iM5
+     *  91  .    ;:.,1&@@@@@@MXs.                            .          .,,:,:&S
+     *  hS ....  .:;,,,i3MMS1;..,..... .  .     ...                     ..,:,.99
+     *  ,8; ..... .,:,..,8Ms:;,,,...                                     .,::.83
+     *   s&: ....  .sS553B@@HX3s;,.    .,;13h.                            .:::&1
+     *    SXr  .  ...;s3G99XA&X88Shss11155hi.                             ,;:h&,
+     *     iH8:  . ..   ,;iiii;,::,,,,,.                                 .;irHA
+     *      ,8X5;   .     .......                                       ,;iihS8Gi
+     *         1831,                                                 .,;irrrrrs&@
+     *           ;5A8r.                                            .:;iiiiirrss1H
+     *             :X@H3s.......                                .,:;iii;iiiiirsrh
+     *              r#h:;,...,,.. .,,:;;;;;:::,...              .:;;;;;;iiiirrss1
+     *             ,M8 ..,....,.....,,::::::,,...         .     .,;;;iiiiiirss11h
+     *             8B;.,,,,,,,.,.....          .           ..   .:;;;;iirrsss111h
+     *            i@5,:::,,,,,,,,.... .                   . .:::;;;;;irrrss111111
+     *            9Bi,:,,,,......                        ..r91;;;;;iirrsss1ss1111
+     */
+}
---
+++ app/src/main/java/com/juhezi/coderslife/main/MainActivity.java
@@ -0,0 +1,89 @@
+package com.juhezi.coderslife.main;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.juhezi.coderslife.R;
+import com.juhezi.coderslife.SingleFragmentActivity;
+import com.juhezi.coderslife.databinding.ActMainBinding;
+import com.juhezi.coderslife.entry.Title;
+
+/**
+ * Created by qiao1 on 2017/1/11.
+ */
+public class MainActivity extends SingleFragmentActivity<ActMainBinding> {
+    private static String TAG = "MainActivity";
+
+    private Toolbar mToolbar;
+    private ActionBar mActionBar;
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
+    private Title title = new Title(Title.TODAY_TITLE);
+
+    @Override
+    protected Fragment getFragment() {
+        return MainFragment.Companion.getInstance();
+    }
+
+    @Override
+    protected int getActLayoutRes() {
+        return R.layout.act_main;
+    }
+
+    @Override
+    protected int getFragContainerId() {
+        return R.id.rl_act_main_frag;
+    }
+
+    @Override
+    protected void init(ActMainBinding binding) {
+        super.init(binding);
+        initView();
+        initActionBar();
+        initData(binding);
+    }
+
+    private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_act_main);
+        mNavigationView = (NavigationView) findViewById(R.id.nv_act_main);
+    }
+
+    private void initActionBar() {
+        mToolbar = (Toolbar) findViewById(R.id.tb_act_main);
+        setSupportActionBar(mToolbar);
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(null);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.drawer_layout_open_desc,
+                R.string.drawer_layout_close_desc);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initData(ActMainBinding binding) {
+        binding.setTitle(title);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
