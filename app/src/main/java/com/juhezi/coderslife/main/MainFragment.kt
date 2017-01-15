b2970de1a2798fd3f0ab9120a9dbf7c7f4211401
Juhezi <juhezix@163.com>
2017-01-15 21:56:39 +0800

    add the navgation to select which note

--- app/src/main/java/com/juhezi/coderslife/main/MainFragment.kt
+++ app/src/main/java/com/juhezi/coderslife/main/MainFragment.kt
@@ -3,12 +3,13 @@ package com.juhezi.coderslife.main
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.juhezi.coderslife.R
-import com.juhezi.coderslife.test.TestAct
 
 /**
  * Created by qiao1 on 2017/1/9.
@@ -28,12 +29,17 @@ class MainFragment : Fragment() {
         return rootView
     }
 
-    private var fabAdd: FloatingActionButton? = null
+    private var mFabAdd: FloatingActionButton? = null
+    private var mCvNavWrapper: CardView? = null
+    private var mNavAdd: NavigationView? = null
+    private var mVOverlay: View? = null
 
     private fun initEvent(rootView: View?) {
-        fabAdd = rootView?.findViewById(R.id.fab_add) as FloatingActionButton
-        fabAdd?.setOnClickListener {
-//            startActivity(Intent(context, TestAct::class.java))
+        mFabAdd = rootView?.findViewById(R.id.fab_add) as FloatingActionButton
+        mNavAdd = rootView?.findViewById(R.id.nav_frag_main_add_item) as NavigationView?
+        mCvNavWrapper = rootView?.findViewById(R.id.nav_frag_main_add_item) as CardView?
+        mFabAdd?.setOnClickListener {
+
         }
     }
 
