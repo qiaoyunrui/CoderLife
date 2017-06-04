e16b0b2bc709eb642844fe8f57ca10bedb1d4cf1
Juhezi <juhezix@163.com>
2017-06-04 20:36:16 +0800

    Add the Intent Utils

---
+++ base/src/main/java/com/juhezi/module/base/util/IntentUtils.java
@@ -0,0 +1,31 @@
+package com.juhezi.module.base.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+/**
+ * Created by Juhezi[juhezix@163.com] on 2017/6/4.
+ */
+
+public class IntentUtils {
+
+    public static String DEFAULT_TYPE = "*/*";
+
+    public static String IMAGE_TYPE = "image/*";
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static Intent getOpenDocumentIntent(String type) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType(type);
+        return intent;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static Intent getOpenDocumentIntent() {
+        return getOpenDocumentIntent(DEFAULT_TYPE);
+    }
+
+}
--- test/src/main/java/com/juhezi/test/MainActivity.java
+++ test/src/main/java/com/juhezi/test/MainActivity.java
@@ -1,6 +1,8 @@
 package com.juhezi.test;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.Button;
 
 import com.juhezi.module.base.router.JUri;
 import com.juhezi.module.base.router.operator.manager.OMP;
+import com.juhezi.module.base.util.IntentUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Button button2 = (Button) findViewById(R.id.btn_turn_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                startActivity(IntentUtils.getOpenDocumentIntent(IntentUtils.IMAGE_TYPE));
+            }
+        });
+
     }
 }
