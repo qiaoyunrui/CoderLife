b26d2f3f7e281aede8376f10e56c4cedce87cac7
Juhezi <juhezix@163.com>
2017-06-11 17:46:31 +0800

    Update the Gradle

--- dailytasks/src/main/java/com/juhezi/dailytasks/DailyTasksApplication.java
+++ dailytasks/src/main/java/com/juhezi/dailytasks/DailyTasksApplication.java
@@ -1,6 +1,8 @@
 package com.juhezi.dailytasks;
 
+import com.juhezi.dailytasks.tasks.TasksActivity;
 import com.juhezi.module.base.common.BaseApplication;
+import com.juhezi.module.base.router.operator.manager.OMP;
 
 /**
  * Created by Juhezi[juhezix@163.com] on 2017/6/5.
@@ -8,4 +10,9 @@ import com.juhezi.module.base.common.BaseApplication;
 
 public class DailyTasksApplication extends BaseApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OMP.putURI(TasksActivity.URL, TasksActivity.class);
+    }
 }
--- dailytasks/src/main/java/com/juhezi/dailytasks/tasks/TasksActivity.java
+++ dailytasks/src/main/java/com/juhezi/dailytasks/tasks/TasksActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 
 import com.juhezi.dailytasks.R;
 import com.juhezi.module.base.common.SingleFragmentActivity;
+import com.juhezi.module.base.router.JUri;
 
 public class TasksActivity extends SingleFragmentActivity {
+
+    public static final JUri URL = new JUri(JUri.ACTIVITY, TasksActivity.class.getName());
 
     @Override
     protected Fragment createFragment() {
@@ -27,6 +30,5 @@ public class TasksActivity extends SingleFragmentActivity {
     @Override
     protected void init(@Nullable Bundle savedInstanceState) {
         super.init(savedInstanceState);
-
     }
 }
